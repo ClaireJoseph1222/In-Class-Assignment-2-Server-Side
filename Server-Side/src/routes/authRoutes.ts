3604ac0a4948949d1ff0e2ced124c0f46c2b91b4
ClaireJoseph1222 <157498308+ClaireJoseph1222@users.noreply.github.com>
2026-03-26 11:50:19 -0400

    Delete Server-Side directory

--- Server-Side/src/routes/authRoutes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { registerHandler, loginHandler } from "../controllers/authController";
-
-export const authRouter = Router();
-
-authRouter.post("/register", registerHandler);
-authRouter.post("/login", loginHandler);
